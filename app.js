a07c6f560020be204792bb7370f571d6983d1630
zhouguangxing <749106618@qq.com>
2019-09-17 17:59:04 +0800

    星期三

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 //app.js
 App({
   onLaunch: function () {
-    // let token = wx.getStorageSync(token);
-    // if (token) {
-    //   return;
-    // } else {
-    //   wx.reLaunch({
-    //     url: '/pages/login/login'
-    //   })
-    // }
+
   },
   globalData: {
 
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,44 @@
+let { request } = require('../../utils/api.js')
+var app = getApp()
 Page({
     onLoad(){
-        let token = wx.getStorageSync(token);
-        if (token) {
-        return;
-        } else {
-        wx.reLaunch({
-            url: '/pages/login/login'
-        })
-        }
+        wx.login({
+            success: code => {
+              console.log(code);
+              let wxCode = {
+                wxcode: code.code
+              }
+              wx.getSetting({
+                success(res) {
+                  console.log(res);
+                  // 登录 已经授权，可以直接调用 getUserInfo 获取头像昵称
+                  if (res.authSetting['scope.userInfo']) {             
+                    wx.getUserInfo({
+                      success: userInfoResult => {
+                        // 可以将 res 发送给后台解码出 unionId
+                        console.log(userInfoResult);
+                        wxCode.nickName = userInfoResult.userInfo.nickName;
+                        wxCode.gender  = userInfoResult.userInfo.gender;
+                        wxCode.headImgUrl = userInfoResult.userInfo.avatarUrl;
+                        request.apiPost('/api/v1/user/wxlogin', wxCode).then(result => {
+                          console.log("得到用户信息", result)
+                          sessionKey = result.data.sessionKey
+                        })
+                      }
+                    })
+                  }else{
+                    console.log(1);
+                    
+                    wx.reLaunch({
+                        url:'/pages/login/login'
+                    })
+                        
+                  }
+                }
+              })
+      
+            }
+          })
+          
     }
 })
